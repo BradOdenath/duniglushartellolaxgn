712ec5560a678fa079b13c8f73b49c294f54a760
BradOdenath <brad.odenath@gmail.com>
2021-05-27 06:03:55 -0400

    l

---
+++ references/aquarium_system.lua
@@ -0,0 +1,37 @@
+anerobic_bacteria = {
+    data = {};
+    name = 'anerobic_bacteria';
+    enumerations = {
+        values = {};
+        extension = {
+            constants = {};
+            finals = {};
+            attributes = {};
+            functions = {};
+        };
+    };
+    static = {
+        constants = {};
+        finals = {};
+        attributes = {};
+        functions = {};
+    };
+    constants = {};
+    finals = {};
+    attributes = {
+        ['dimensions'] = 'DimensionsReference';
+        ['tanks'] = 'Map<dynamic,TankReference>';
+    };
+    accessors = {};
+    mutators = {};
+    functions = {
+        
+    };
+};
+
+function to()
+    print('importing anerobic_bacteria reference data')
+    return anerobic_bacteria
+end
+
+to()
